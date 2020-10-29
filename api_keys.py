ed3ab8ab7d2b6bca6b9048f40211fae611c4e7ad
SamHoney71 <gjeter91@gmail.com>
2020-10-28 23:39:52 -0500

    random city selection works

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "a4e427d4d3b2b9f664febe81ebd85714"
+
+# Google API Key
+g_key = "AIzaSyDfbD7O24VnnVVs-xIM3gFRCo6AdiTWHic"
---
+++ config.py
@@ -0,0 +1,1 @@
+api_key = "a4e427d4d3b2b9f664febe81ebd85714"
